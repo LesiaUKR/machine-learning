acf985e308869cb31696417f0b0e5eadbe11f56c
Lesia Soloviova <solovyova.lesya@gmail.com>
2024-09-21 20:10:30 +0100

    5 lesson comments added

--- homework/dz_mod_02_Lesia_Soloviova.py
+++ homework/dz_mod_02_Lesia_Soloviova.py
@@ -1,6 +1,5 @@
 # 1. імпортуємо необхідні пакети
 
-import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
@@ -42,9 +41,20 @@ target_cleaned = target[outliers_mask]
 print(f'Видалено {len(data) - len(data_cleaned)} рядків з аномальними значеннями.')
 
 #%%
-# 3.2 Видалення високо корельованої ознаки - видалимо 'AveBedrms',
-# яка має високу кореляцію з 'AveRooms'
-data_cleaned = data_cleaned.drop('AveBedrms', axis=1)
+# 3.2 Видалення високо корельованої ознаки
+# Побудуємо кореляційну матрицю після очистки від викидів
+corr_matrix = data_cleaned.drop(['Longitude', 'Latitude'], axis=1).corr()
+
+# Візуалізація кореляційної матриці
+plt.figure(figsize=(10, 8))
+sns.heatmap(corr_matrix, annot=True, cmap='coolwarm')
+plt.show()
+
+# Видалимо ознаку з найвищою кореляцією AveRooms, яка має високу кореляцію
+# з іншою ознакою AveBedrms
+
+data_cleaned = data_cleaned.drop('AveRooms', axis=1)
+
 
 #%%
 # 4. Розподілимо дані на навчальні та тестові вибірки
@@ -58,7 +68,7 @@ X_train_scaled = scaler.fit_transform(X_train)
 X_test_scaled = scaler.transform(X_test)
 
 #%%
-# 6. Побудуємо моделі та оцінимо метрики
+# 6. Побудова та навчання моделі
 model = LinearRegression()
 model.fit(X_train_scaled, y_train)
 
@@ -106,5 +116,4 @@ print(f"Нові результати:     R2: {r_sq:.2f} | MAE: {mae:.2f} | MAP
 нормалізація, позитивно вплинула на модель, хоч і не дуже значно.
 
 '''
-
 
